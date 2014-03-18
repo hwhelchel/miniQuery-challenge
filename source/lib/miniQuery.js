5e1a387ec72b7e590a0e6ea8e9324a2295712a38
& <pair+harryw377.hpchess@devbootcamp.com>
2014-03-18 11:18:07 -0700

    Add show and hide functions.
    Store initial display style when hiding element.
    When showing element reapply initial display style.

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -33,3 +33,41 @@ var SweetSelector = (function(){
   }
 
 }())
+
+var DOM = (function(S){
+
+  var _initialDisplayProperty = {};
+
+  var _hide = function(selected, selection){
+    if (selected.length) {
+      _initialDisplayProperty[selection] = []
+      for (i=0; i < selected.length; i++) {
+        _initialDisplayProperty[selection].push(selected[i].style.display);
+        selected[i].style.display = 'none';
+      }
+    } else {
+      _initialDisplayProperty[selection] = selected.style.display;
+      selected.style.display = 'none';
+    }
+  }
+
+  var _show = function(selected, selection){
+    if (selected.length){
+      for (i=0; i < selected.length; i++) {
+        selected[i].style.display = _initialDisplayProperty[selection][i];
+      }
+    } else {
+      selected.style.display = _initialDisplayProperty[selection];
+    }
+  }
+
+  return {
+    hide: function(selection){
+      _hide(S.select(selection), selection);
+    },
+    show: function(selection){
+      _show(S.select(selection), selection);
+    }
+  }
+
+}(SweetSelector))
