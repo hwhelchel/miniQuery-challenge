253b3a56398ee40830c9160836feef8f9cb3198e
& <pair+harryw377.hpchess@devbootcamp.com>
2014-03-18 10:23:59 -0700

    Add selection methods for SweetSelector

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,35 @@
 /*!
  * minQuery
  */
+
+var SweetSelector = (function(){
+
+  var _IdSelector = function(id){
+    return document.getElementById(id);
+  };
+
+  var _ClassSelector = function(klass){
+    return document.getElementsByClassName(klass);
+  }
+
+  var _TagSelector = function(tag){
+    return document.getElementsByTagName(tag);
+  }
+
+  var _CheckSelectType = function(selection){
+    if (selection.charAt(0) === ".") {
+      return _ClassSelector(selection.substr(1));
+    } else if (selection.charAt(0) === "#") {
+      return _IdSelector(selection.substr(1));
+    } else {
+      return _TagSelector(selection);
+    }
+  }
+
+  return {
+    select: function(selection){
+      return _CheckSelectType(selection);
+    }
+  }
+
+}())
